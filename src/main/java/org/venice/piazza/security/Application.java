5df3fa39b97f506294588ee033fbeea65146d67a
Russell Orf <russellorf@datamentors.com>
2016-05-23 14:05:08 -0400

    Adding CORS header

--- src/main/java/org/venice/piazza/security/Application.java
+++ src/main/java/org/venice/piazza/security/Application.java
@@ -19,7 +19,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @ComponentScan({ "org.venice.piazza.security, util" })
@@ -33,4 +41,28 @@ public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@Configuration
+	protected static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.addFilterBefore(corsFilter(), ChannelProcessingFilter.class).csrf().disable();
+		}
+
+		@Bean
+		public CorsFilter corsFilter() {
+			UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+			CorsConfiguration config = new CorsConfiguration();
+			config.addAllowedOrigin("*");
+			config.addAllowedHeader("*");
+			config.addAllowedMethod("OPTIONS");
+			config.addAllowedMethod("GET");
+			config.addAllowedMethod("PUT");
+			config.addAllowedMethod("POST");
+			config.addAllowedMethod("DELETE");
+			source.registerCorsConfiguration("/**", config);
+			return new CorsFilter(source);
+		}
+	}	
 }
