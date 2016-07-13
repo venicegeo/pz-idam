660cd5fc112d3dd3de3ee17d6f13e97fe8545d86
Russell Orf <russellorf@datamentors.com>
2016-07-13 15:15:13 -0400

    Removing unnecessary model.

--- src/main/java/org/venice/piazza/security/data/MongoAccessor.java
+++ src/main/java/org/venice/piazza/security/data/MongoAccessor.java
@@ -74,32 +74,4 @@ public class MongoAccessor {
 		}
 		return null;
 	}
-
-	public class UUIDAssignment {
-
-		private String uuid;
-
-		private String username;
-
-		public UUIDAssignment(String username, String uuid) {
-			setUsername(username);
-			setUuid(uuid);
-		}
-
-		public String getUuid() {
-			return uuid;
-		}
-
-		public void setUuid(String uuid) {
-			this.uuid = uuid;
-		}
-
-		public String getUsername() {
-			return username;
-		}
-
-		public void setUsername(String username) {
-			this.username = username;
-		}
-	}
 }
