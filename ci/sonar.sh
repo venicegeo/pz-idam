c152147590eeeebfe268b9be00c7bbe246b1f3bf
Russell Orf <russellorf@datamentors.com>
2017-05-03 15:39:14 -0400

    orfr updating ci/sonar.sh

--- ci/sonar.sh
+++ ci/sonar.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -ex
 
-pushd `dirname $0`/.. > /dev/null
 root=$(pwd -P)
-popd > /dev/null
+mkdir -p $root/.m2/repository
 
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install -Pcoverage-per-test org.jacoco:jacoco-maven-plugin:report -DdataFile=$root/target/jacoco.exec
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install -Pcoverage-per-test org.jacoco:jacoco-maven-plugin:report -DdataFile=$root/target/jacoco.exec -Dmaven.repo.local="$root/.m2/repository"
