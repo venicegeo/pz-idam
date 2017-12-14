7eb8ed66530ef54a906b8db2dbdc08b42767904f
John McMahon <jmcmahon@radiantblue.com>
2017-12-13 20:15:03 -0500

    Fix string format

--- src/main/java/org/venice/piazza/idam/controller/AuthController.java
+++ src/main/java/org/venice/piazza/idam/controller/AuthController.java
@@ -435,7 +435,7 @@ public class AuthController {
 				cookie.setDomain(domain);
 				response.addCookie(cookie);
 
-				return new RedirectView(String.format("https://{}?logged_in=true", oAuthClient.getUiUrl(request)));
+				return new RedirectView(String.format("https://%s?logged_in=true", oAuthClient.getUiUrl(request)));
 			} catch (HttpClientErrorException | HttpServerErrorException hee) {
 				LOGGER.error(hee.getResponseBodyAsString(), hee);
 				RedirectView errorView = new RedirectView();
